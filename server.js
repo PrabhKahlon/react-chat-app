7c72f1090d3e909cd39817f45685f777a81d40c3
Prabh Kahlon <prabhk@sfu.ca>
2021-06-17 02:07:03 -0700

    added socket.io connection and join room

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
-const express = require ("express");
-const PORT = 3000 || process.env.PORT;
+const express = require("express");
+const PORT = 5000 || process.env.PORT;
 const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+
+var users = [];
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
-app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
+app.get("/api/login", (req, res) => {
+
+});
+
+io.on("connection", (socket) => {
+    console.log("new client connection");
+    console.log(socket.id);
+    io.to(socket.id).emit("userList", users);
+    socket.on("newUser", (name) => {
+        const user = {id: socket.id, name: name};
+        users.push(user);
+        socket.broadcast.emit("addUser", {id: socket.id, name: name});
+    });
+});
+
+server.listen(PORT, () => console.log(`Server listening on ${PORT}`));
